523b0aedc71a3f13dffaa6451031f0f971cdf26e
bhanuteja.a <bhanuteja.a@tadigital.com>
2020-10-25 13:02:22 +0530

    first commit

--- src/components/SecondPage.vue
+++ src/components/SecondPage.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <h2>
-      Second page
-    </h2>
+    <h2>Second page</h2>
     <button @click="back">back</button>
+    <div>
+      <b-table striped hover :items="items"></b-table>
+    </div>
   </div>
 </template>
 
@@ -14,6 +15,16 @@ export default {
     back () {
       this.$router.go(-1);
     }
-  }
+  },
+  data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }
 }
 </script>
